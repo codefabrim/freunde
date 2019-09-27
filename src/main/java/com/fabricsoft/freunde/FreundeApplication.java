8dcf25fdc4463048c33f9eb89656cf26e7bb4cf3
fabgitlocal <fmiredin7@gmail.com>
2019-09-27 13:01:09 +0200

    freunde project: initial configuration get started

---
+++ src/main/java/com/fabricsoft/freunde/FreundeApplication.java
@@ -0,0 +1,14 @@
+package com.fabricsoft.freunde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreundeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FreundeApplication.class, args);
+	}
+
+}
